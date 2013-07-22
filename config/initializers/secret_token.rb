32c86b327b9f13aa761a1fdd140b751245aec020
Vanessa Vengco <vengco@gmail.com>
2013-07-22 10:21:36 -0700

    Created base rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StreetBeat::Application.config.secret_key_base = '54a5d8b618bf01d9e57653306cd23627fd9d978d2cec5ff00c70c67a57a1101d7b9b3bf4d9644f5db75398b3d35eb0b0f1e4a5dd7402363d3ef456b78283c302'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StreetBeat::Application.config.session_store :cookie_store, key: '_street_beat_session'
