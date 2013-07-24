a16e791d4430a7f620adb8e6bbcbdae76c7ff2fd
Vanessa Vengco <vengco@gmail.com>
2013-07-23 19:36:45 -0700

    Clone link_block of link inputs in new Acts view onclick

--- app/assets/javascripts/acts.js
+++ app/assets/javascripts/acts.js
@@ -1,6 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$('.addlink').on('click', function(){
-	$('#linkbox').clone();
-})
+// Clone input boxes for education block
+	$('.link_block_add').click(function() {
+		var html = $('.link_block').first().clone();
+		html.css('display', 'none');
+		html.find('input').val('');
+		$(this).before(html);
+
+	// $('.help_block').before(html);
+		html.slideDown(600);
+		return false;
+	});
