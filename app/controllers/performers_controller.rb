75de7779bb480c9648dd14bdb4d0ed17ab6b43ec
boovius <joshua.book@gmail.com>
2013-07-24 18:51:52 -0700

    fix performers and concerts controllers and routes

---
+++ app/controllers/performers_controller.rb
@@ -0,0 +1,11 @@
+class PerformersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,14 @@
 StreetBeat::Application.routes.draw do
   
- 
-
-  get "concert/index"
-  get "concert/new"
-  get "concert/edit"
-  get "concert/show"
-  post "concert/create"
-  put  "concert/update"
-
-
-  get "performer/index"
-  get "performer/new"
-  get "performer/edit"
-  get "performer/show"
-  post "performer/create"
-  put  "performer/update"
-  
   get "login" => "session#new"
   post "login" => "session#create"
   delete "logout" => "session#destroy"
   get "logout" => "session#destroy"
   
   resources :users
+  resources :concerts
+  resources :performers 
+
 
   get "privacy" => "site#privacy"
   get "terms" => "site#terms"
