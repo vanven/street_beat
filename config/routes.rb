65db32c1926ba768a93f5286fcffd3d9850b5f02
boovius <joshua.book@gmail.com>
2013-07-24 17:37:52 -0700

    add new concert and new performer forms

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,17 @@ StreetBeat::Application.routes.draw do
   get "concert/new"
   get "concert/edit"
   get "concert/show"
+  post "concert/create"
+  put  "concert/update"
+
+
   get "performer/index"
   get "performer/new"
   get "performer/edit"
+  get "performer/show"
+  post "performer/create"
+  put  "performer/update"
+  
   get "login" => "session#new"
   post "login" => "session#create"
   delete "logout" => "session#destroy"
