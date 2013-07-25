6f16fc3df762670ec1a9f208e8e5a31fa688ea7e
boovius <joshua.book@gmail.com>
2013-07-25 12:28:54 -0700

    add duration to hours and mins and cover to money methods in concert model and adjust concert index and show views accordingly

--- spec/models/concert_spec.rb
+++ spec/models/concert_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe Concert do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#duration_to_s" do
+  	
+  	context "duration is 240" do
+	  	specify "returns 4 hours as a string" do
+	  		concert = Concert.new(duration: 240)
+	  		expect(concert.duration_to_s).to eql('4 hours')
+	  	end
+		end
+
+		context "duration is 244" do
+	  	specify "returns 4 hours and 4 minutes as a string" do
+	  		concert = Concert.new(duration: 244)
+	  		expect(concert.duration_to_s).to eql('4 hours and 4 minutes')
+	  	end
+	  end
+	end
+
+	describe "#cover_to_money" do
+		context "cover charge is 5000" do
+			specify "returns $50.00" do
+				concert = Concert.new(cover_charge: 5000)
+				expect(concert.cover_to_money).to eql('$50.00')
+			end
+		end
+
+		context "cover charge is 55599" do
+			specify "returns $55.99" do
+				concert = Concert.new(cover_charge: 5599)
+				expect(concert.cover_to_money).to eql('$55.99')
+			end
+		end
+	end
+
 end
