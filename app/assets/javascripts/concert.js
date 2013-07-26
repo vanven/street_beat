5e89caceafdb4ddce6991a2cafcfe383b2acd009
boovius <joshua.book@gmail.com>
2013-07-26 11:55:09 -0700

    add adding more timeslots to create concert form

--- app/assets/javascripts/concert.js
+++ app/assets/javascripts/concert.js
@@ -1,40 +1,8 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
 
 $(function() {
 
-// Below is the name of the textfield that will be autocomplete    
-$('.concert_performers').autocomplete({
-	minLength: 3,
-  source: function(request, response){
-           $.ajax({
-               type: "GET",
-               url: "../performers",
-               contentType: "application/json; charset=utf-8",
-               dataType: "json", 
-               data: {term: $('#concert_performers').val()},
-               success: function (msg) {
-                   console.log(msg);
-                   response(msg);
-               },
-               error: function (msg) {
-                   alert(msg.status + ' ' + msg.statusText);
-               }
-           })
-       },
-  focus: function(event, ui) {
-      $('#concert_performers').val(ui.item.name);
-      return false;
-  },
-  select: function(event, ui) {
-    $('#concert_performers').val(ui.item.name);
-    $('#performer_id').val(ui.item.id);
-            return false;
-        }
-	}).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
-	    return $( "<li></li>" ).data( "item.autocomplete", item ).
-	    append( "<a>" + item.name + "</a>" ).appendTo( ul );
-	};
+	performerAutoComplete();
+
 
 
 	$('#concert_location').autocomplete({
@@ -70,5 +38,51 @@ $('.concert_performers').autocomplete({
 	};
 
 
-	
+	$('.time_slot_add').click(function() {
+		var html = $('.time_slot_block').first().clone();
+		html.css('display', 'none');
+		html.find('input').val('');
+		$(this).before(html);
+
+	// $('.help_block').before(html);
+		html.slideDown(600);
+		performerAutoComplete();
+		return false;
+	});
+
+
 }); /*end anonymous function */
+
+function performerAutoComplete(){
+	$('.concert_performers').autocomplete({
+	minLength: 3,
+  source: function(request, response){
+           $.ajax({
+               type: "GET",
+               url: "../performers",
+               contentType: "application/json; charset=utf-8",
+               dataType: "json", 
+               data: {term: $('.concert_performers').val()},
+               success: function (msg) {
+                   console.log(msg);
+                   response(msg);
+               },
+               error: function (msg) {
+                   alert(msg.status + ' ' + msg.statusText);
+               }
+           })
+       },
+  focus: function(event, ui) {
+      $('.concert_performers').val(ui.item.name);
+      return false;
+  },
+  select: function(event, ui) {
+    $('.concert_performers').val(ui.item.name);
+    $('#performer_id').val(ui.item.id);
+            return false;
+        }
+	}).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+	    return $( "<li></li>" ).data( "item.autocomplete", item ).
+	    append( "<a>" + item.name + "</a>" ).appendTo( ul );
+	};
+} /* end function performerAutoComplete */
