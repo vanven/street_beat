8e62267c90f86ec0c21963b9e68786028a0ff217
boovius <joshua.book@gmail.com>
2013-07-30 17:19:33 -0700

    complete location create form - no here button functionality yet

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -19,8 +19,26 @@ class LocationsController < ApplicationController
   end
 
   def new
+    @location = Location.new
+  end
+
+  def create 
+    @location = Location.new(location_params)
+
+    if @location.save
+      redirect_to concert_new_path
+    else
+      render :new
+    end
+
   end
 
   def edit
+  end
+
+    private
+  
+  def location_params    
+    params.require(:location).permit(:name, :street_lin1, :street_line2, :zip_code, :lat_lng)
   end
 end
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,6 +4,16 @@ class SiteController < ApplicationController
 
     @concerts = Concert.all 
 
+    @concertsjs = @concerts.map do |concert|
+      lat_lng = concert.location.lat_lng.split(", ")
+      "{" +
+      "lat: #{lat_lng[0]}," +
+      "lng: #{lat_lng[1]}," +
+      "title: '#{concert.name}'," +
+      "id: #{concert.id}" +
+      "}"
+    end.join(",").html_safe
+
     @concerts.each do |c| 
       puts c
       puts c.location.lat_lng
